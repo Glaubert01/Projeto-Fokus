a8feec39ae37e0deedcf2e63e6e16082a88132c3
Glaubert01 <glaubert@outlook.com>
2025-08-24 10:03:07 -0300

    feat(localStorage): armazenar tarefa em localStorage

--- script-crud.js
+++ script-crud.js
@@ -1,4 +1,18 @@
-<head>
-    <script src="./script.js" defer></script>
-    <script src="./script-crud.js" defer></script>
-</head>
+// encontra o botao adicionar tarefa
+const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
+const formAdicionarTarefa = document.querySelector('.app__form-add-task');
+const texteArea = document.querySelector('.app__form-textarea');
+
+const tarefas = [];
+btnAdicionarTarefa.addEventListener('click', () => {
+    formAdicionarTarefa.classList.toggle('hidden');
+});
+
+formAdicionarTarefa.addEventListener('submit', (evento) => {
+    evento.preventDefault();
+    const tarefa = {
+        descricao: texteArea.value,
+    };
+    tarefas.push(tarefa);
+    localStorage.setItem('tarefas', JSON.stringify(tarefas));
+});
