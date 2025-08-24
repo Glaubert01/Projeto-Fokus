0a8ab6c1a705bee302322e6ced4fb6149d65669b
Glaubert01 <glaubert@outlook.com>
2025-08-24 10:44:40 -0300

    feat(lista de tarefas): adicao de tarefas a lista finalizada

--- script-crud.js
+++ script-crud.js
@@ -2,35 +2,38 @@
 const btnAdicionarTarefa = document.querySelector('.app__button--add-task');
 const formAdicionarTarefa = document.querySelector('.app__form-add-task');
 const texteArea = document.querySelector('.app__form-textarea');
+const ulTarefas = document.querySelector('.app__section-task-list');
 
-const tarefas = localStorage.getItem('tarefas')
+const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [];
 
 function criarElementoTarefa(tarefa) {
     const li = document.createElement('li');
-    li.classList.add('app__section-tak-list-item');
+    li.classList.add('app__section-task-list-item');
 
-    const svg = document.createElementNS('svg');
+    const svg = document.createElement('svg');
     svg.innerHTML = `
-      <svg>
-        <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <svg class="app__section-task-icon-status" width="24" height="24" viewBox="0 0 24 24" fill="none" 
+                            xmlns="http://www.w3.org/2000/svg">
             <circle cx="12" cy="12" r="12" fill="#FFF"></circle>
             <path d="M9 16.1719L19.5938 5.57812L21 6.98438L9 18.9844L3.42188 13.4062L4.82812 12L9 16.1719Z" fill="#01080E"></path>
         </svg>
-    </svg>
     `
 
     const paragrafo = document.createElement('p');
     paragrafo.textContent = tarefa.descricao;
+    paragrafo.classList.add('app__section-task-list-item-description');
 
     const botao = document.createElement('button');
+    botao.classList.add('app_button-edit');
     const imagemBotao = document.createElement('img');
     imagemBotao.setAttribute('src', '/imagens/edit.png');
-    botao.appendChild(imagemBotao);
+    botao.append(imagemBotao);
 
-    li.appendChild(svg);
-    li.appendChild(paragrafo);
-    li.appendChild(botao);
+    li.append(svg);
+    li.append(paragrafo);
+    li.append(botao);
 
+    return li;
 }
 btnAdicionarTarefa.addEventListener('click', () => {
     formAdicionarTarefa.classList.toggle('hidden');
@@ -42,5 +45,14 @@ formAdicionarTarefa.addEventListener('submit', (evento) => {
         descricao: texteArea.value,
     };
     tarefas.push(tarefa);
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
     localStorage.setItem('tarefas', JSON.stringify(tarefas));
+    texteArea.value = '';
+    formAdicionarTarefa.classList.add('hidden');
+});
+
+tarefas.forEach(tarefa => {
+    const elementoTarefa = criarElementoTarefa(tarefa);
+    ulTarefas.append(elementoTarefa);
 });
